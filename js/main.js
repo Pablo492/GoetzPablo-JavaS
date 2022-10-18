79051071af013b52361f463d8a88e0b0456876ad
Pablo Goetz <goetz.pablo@gmail.com>
2022-10-18 20:10:20 -0300

    sumando al proyecto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//Introduccion a la pagina
+alert("Bienvenidos a Multimarca Accesorios")
+
+let nombre = prompt ("Ingrese su Nombre o presione 'f' para finalizar")
+while(nombre != "f"|"F"){
+    alert("Hola"+nombre);
+}
+
 
 function correr(){
     let marca = prompt("Por favor ingrese la marca de su automovil: Honda, Ford, Chevrolet, Isuzu o Peugeot");
@@ -11,9 +19,45 @@ if(marca === "Honda"|"Ford"|"Chevrolet"|"Peugeot"|"Isuzu"){
     alert("No trabajamos la marca ingresada");
 }} 
 
-//Precios
-let cantidad = parseInt(prompt("Ingrese cantidad de productos"));
-let precio = $100;
-let total = + cantidad + (+ precio);
 
-alert("El total a pagar es " total);
+
+//Bucle o Ciclo
+let producto = prompt("Ingrese producto o la tecla 'F' para finalizar");
+//le damos permiso que ingrese productos hasta ingresar "F" fin.
+while(producto != "F"|"f"){
+    alert("Se ingreso"+ producto);
+    producto= prompt("Ingresar otro producto o 'f' para finalizar");
+};
+
+
+
+//Precios  (FOR)
+
+let cantidad = parseInt(prompt("Ingrese cantidad de productos"));
+for ( cantidad =1; cantidad > 0; cantidad++) {
+    let precio = $100;
+    let total = (cantidad * precio);
+    alert ("El total a pagar es" = total);
+};
+
+
+//calculo de envio a pagar segun cantidad comprada
+var precioTransporteAPagar;
+
+if (numProductos <= 2){
+precioTransporteAPagar = precioTransporte[0];
+}
+else if (numProductos <= 3){
+precioTransporteAPagar = precioTransporte[1];
+}
+else if (numProductos <= 4){
+precioTransporteAPagar = precioTransporte[2];
+}
+else if (numProductos >= 5){
+precioTransporteAPagar = precioTransporte[3];
+}
+
+//
+
+//Ciclo Bucle
+for (i=1; i<10; i++);
